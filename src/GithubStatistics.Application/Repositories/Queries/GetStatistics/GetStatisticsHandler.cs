e5f22faf0265dcb401f8a9ae3932fb42258f6e67
Rafał Schmidt <rafalschmidt97@gmail.com>
2020-04-11 14:12:46 +0200

    feature: implement get statistics logic
    Still missing the connection to github.

--- src/GithubStatistics.Application/Repositories/Queries/GetStatistics/GetStatisticsHandler.cs
+++ src/GithubStatistics.Application/Repositories/Queries/GetStatistics/GetStatisticsHandler.cs
@@ -1,4 +1,5 @@
-using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using GithubStatistics.Application.Core.Bus;
 using GithubStatistics.Application.Repositories.Infrastructure.Github;
@@ -14,9 +15,48 @@ namespace GithubStatistics.Application.Repositories.Queries.GetStatistics
             _githubFetcher = githubFetcher;
         }
 
-        public override Task<RepositoriesStatistics> Handle(GetStatisticsQuery request)
+        public override async Task<RepositoriesStatistics> Handle(GetStatisticsQuery request)
         {
-            throw new NotImplementedException();
+            var repositories = await _githubFetcher.GetRepositoriesDetails(request.Owner);
+
+            var letters = new Dictionary<char, int>();
+            float sumStargazers = 0,
+                  sumWatchers = 0,
+                  sumForks = 0,
+                  sumSize = 0;
+
+            foreach (var repository in repositories)
+            {
+                FillLetters(repository.Name, letters);
+                sumStargazers += repository.StargazersCount;
+                sumWatchers += repository.WatchersCount;
+                sumForks += repository.ForkCount;
+                sumSize += repository.DiskUsage;
+            }
+
+            return new RepositoriesStatistics(
+                request.Owner,
+                letters,
+                sumStargazers / repositories.Count,
+                sumWatchers / repositories.Count,
+                sumForks / repositories.Count,
+                sumSize / repositories.Count);
+        }
+
+        private static void FillLetters(string repositoryName, IDictionary<char, int> letters)
+        {
+            var simplified = Regex.Replace(repositoryName.ToLowerInvariant(), "[^a-z]", "");
+            foreach (var letter in simplified)
+            {
+                if (letters.ContainsKey(letter))
+                {
+                    letters[letter] += 1;
+                }
+                else
+                {
+                    letters[letter] = 1;
+                }
+            }
         }
     }
 }
